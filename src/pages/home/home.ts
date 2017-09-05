a3b07d04b9eaf781177761ceabbbec78b8e427e2
brittikbasu <brittikbasu@gmail.com>
2017-09-05 17:57:17 +0200

    animate compass when orientation changed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { ScreenOrientation } from '@ionic-native/screen-orientation';
+import {
+  Component
+} from '@angular/core';
+import {
+  NavController
+} from 'ionic-angular';
+import {
+  ScreenOrientation
+} from '@ionic-native/screen-orientation';
 
 
 @Component({
@@ -8,24 +14,58 @@ import { ScreenOrientation } from '@ionic-native/screen-orientation';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+pp : boolean;
+lp : boolean;
   constructor(public navCtrl: NavController, private screenOrientation: ScreenOrientation) {
+this.pp = false;
+this.lp = false;
 
-// get current
-console.log(this.screenOrientation.type); // logs the current orientation, example: 'landscape'
+    // get current
+    console.log(this.screenOrientation.type); // logs the current orientation, example: 'landscape'
 
-// set to landscape
-//this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+    // set to landscape
+    //this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+    if (this.screenOrientation.type == 'portrait-primary') {
+      console.log("potrait-primary");
+      this.pp = true;
+      
+    }
 
-// allow user rotate
-this.screenOrientation.unlock();
+    // allow user rotate
+    this.screenOrientation.unlock();
 
-// detect orientation changes
-this.screenOrientation.onChange().subscribe(
-   () => {
-       console.log("Orientation Changed");
-   }
-);
+    // detect orientation changes
+    this.screenOrientation.onChange().subscribe(
+      () => {
+        console.log("Orientation Changed");
+        
+
+        if (this.screenOrientation.type == 'landscape-primary') {
+          console.log("landscape-primary");
+          this.pp = false;
+          this.lp = true;
+          
+        } else if (this.screenOrientation.type == 'landscape-secondary') {
+          console.log("landscape-secondary");
+          this.pp = false;
+          this.lp = false;
+          
+        }
+        else if (this.screenOrientation.type == 'portrait-primary') {
+          console.log("potrait-primary");
+          this.lp = false;
+          this.pp = true;
+          
+        }
+        else if (this.screenOrientation.type == 'portrait-secondary') {
+          console.log("potrait-secondary");
+          this.pp = true;
+          this.lp = false;
+          
+          
+        }
+      }
+    );
 
   }
 
