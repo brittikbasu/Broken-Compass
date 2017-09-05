049b23455f77a47891dc0f6cac9988329f322118
brittikbasu <brittikbasu@gmail.com>
2017-09-05 09:54:51 +0200

    add screen orientation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private screenOrientation: ScreenOrientation) {
+
+// get current
+console.log(this.screenOrientation.type); // logs the current orientation, example: 'landscape'
+
+// set to landscape
+//this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+
+// allow user rotate
+this.screenOrientation.unlock();
+
+// detect orientation changes
+this.screenOrientation.onChange().subscribe(
+   () => {
+       console.log("Orientation Changed");
+   }
+);
 
   }
+
+
 
 }
